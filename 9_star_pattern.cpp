adb211edd762c91d30ba6c385962eee470897f07
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-05 01:38:16 +0530

    Create 9_star_pattern.cpp

---
+++ 9_star_pattern.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= n-i; j++) {
+            cout << "  ";
+        }
+        for(int j = 0; j <= 2*i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = n-1; i >= 0; i--) {
+        for(int j = 0; j <= n-i; j++) {
+            cout << "  ";
+        }
+        for(int j = 0; j <= 2*i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
+// Output
+
+// 5
+//             *
+//           * * *
+//         * * * * *
+//       * * * * * * *
+//     * * * * * * * * *
+//     * * * * * * * * *
+//       * * * * * * *
+//         * * * * *
+//           * * *
+//             * 
