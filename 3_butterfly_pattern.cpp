2cf57de3a3ef7f9a45ce8fe9c95071c97bbf60c7
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-04 13:12:02 +0530

    Create 3_butterfly_pattern.cpp

---
+++ 3_butterfly_pattern.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(j <= i) cout << "* "; 
+            else cout << "  ";
+        }
+        for(int j = n-1;  j >= 0; j--) {
+            if(j <= i) cout << "* ";
+            else cout << "  "; 
+        }
+        cout << endl;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = n-1; j >= 0; j--) {
+            if(j >= i) cout << "* ";
+            else cout << "  "; 
+        }
+        for(int j = 0; j < n; j++) {
+            if(j >= i) cout << "* ";
+            else cout << "  ";
+        }
+        cout << endl;
+    }
+}
+
+
+//Output
+
+// *                 *
+// * *             * *
+// * * *         * * *
+// * * * *     * * * *
+// * * * * * * * * * *
+// * * * * * * * * * *
+// * * * *     * * * *
+// * * *         * * *
+// * *             * *
+// *                 *
