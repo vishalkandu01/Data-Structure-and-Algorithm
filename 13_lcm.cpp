f12cb6b2778a0fdd6bc59557449c2727d7379441
Vishal Kandu <88443773+vishalkandu01@users.noreply.github.com>
2023-04-11 20:06:54 +0530

    Create 13_lcm.cpp

---
+++ 13_lcm.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int lcm(int a, int b) {
+    int res = max(a, b);
+    while(true) {
+        if(res % a == 0 && res % b == 0) 
+            return res;
+        res++;
+    }
+    return res;
+}
+
+int gcd(int a, int b) {
+    if(b == 0) {
+        return a;
+    }
+    return gcd(b, a%b);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    // cout << lcm(a, b) << endl;
+    int lcm = (a*b) / gcd(a, b);
+    cout << lcm << endl;
+}
